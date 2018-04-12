3e8ecb4e0dacab00ca28690617835ea7cee239fb
Daniel-sims <daniel.sims1994@outlook.com>
2018-04-12 12:18:43 +0100

    Initial design for sign in/up page

--- LeagueCloudCoachDesktop/ViewModel/MainViewModel.cs
+++ LeagueCloudCoachDesktop/ViewModel/MainViewModel.cs
@@ -1,34 +1,24 @@
 using GalaSoft.MvvmLight;
+using LeagueCloudCoachDesktop.ViewModel.Login;
 
 namespace LeagueCloudCoachDesktop.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            CurrentPage = new LoginViewModel();
+        }
+
+        private ViewModelBase _currentPage;
+        public ViewModelBase CurrentPage
+        {
+            get { return _currentPage; }
+            set
+            {
+                if (_currentPage == value) { return; }
+                Set(ref _currentPage, value);
+            }
         }
     }
 }
--- LeagueCloudCoachDesktop/ViewModel/ViewModelLocator.cs
+++ LeagueCloudCoachDesktop/ViewModel/ViewModelLocator.cs
@@ -1,5 +1,6 @@
 using CommonServiceLocator;
 using GalaSoft.MvvmLight.Ioc;
+using LeagueCloudCoachDesktop.ViewModel.Login;
 
 namespace LeagueCloudCoachDesktop.ViewModel
 {
@@ -10,19 +11,20 @@ namespace LeagueCloudCoachDesktop.ViewModel
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
             SimpleIoc.Default.Register<MainViewModel>();
+
+            SimpleIoc.Default.Register<LoginViewModel>();
+            SimpleIoc.Default.Register<LoginSignUpViewModel>();
+            SimpleIoc.Default.Register<LoginSignInViewModel>();
         }
 
-        public MainViewModel Main
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
-        }
-        
-        public static void Cleanup()
-        {
-            // TODO Clear the ViewModels
-        }
+        public MainViewModel Main { get { return ServiceLocator.Current.GetInstance<MainViewModel>(); } }
+
+        public LoginViewModel Login { get { return ServiceLocator.Current.GetInstance<LoginViewModel>(); } }
+
+        public LoginSignUpViewModel LoginSignUp { get { return ServiceLocator.Current.GetInstance<LoginSignUpViewModel>(); } }
+
+        public LoginSignInViewModel LoginSignIn { get { return ServiceLocator.Current.GetInstance<LoginSignInViewModel>(); } }
+
+        public static void Cleanup() { }
     }
 }
